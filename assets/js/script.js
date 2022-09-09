6fc48449ce8f07817c4a542541e437a9dcb66003
vomkaraxioned <omkarv@axioned.com>
2022-09-09 18:42:15 +0530

    js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,38 @@
 /* Author: 
 
 */
-$(".box").click(()=>{
-    alert("heelo");
+let box = $(".box");
+
+function animation () {
+    box.animate({ left:'90%'}, 2000, ()=> {
+        box.css( "background-color", "#ff0");
+    });
+    box.animate({ left:'45%'}, 2000, ()=> {
+        box.css( "background-color", "#0ff" );
+    });
+    box.animate({ left: 0 }, 2000, ()=> {
+        box.css("background-color", "#f00");
+    });
+    box.animate({ left: '45%' }, 2000, ()=> {
+        box.css( "background-color", "#0ff" );
+    });
+    box.animate({ top:0 }, 2000, ()=> {
+        box.css("background-color", "#f0f");
+    });
+    box.animate({ top:'40%' }, 2000, ()=> {
+        box.css("background-color", "#0ff");
+    });
+    box.animate({top:'100%'}, 2000, ()=> {
+        box.css("background-color", "#800");
+    });
+    box.animate({top:'40%'}, 2000, ()=> {
+        box.css("background-color", "#0ff");
+        animation();
+    });
+}
+
+box.click(()=>{
+    animation();
 });
 
 
