77a12321cc0517ea88a3fe7af27b3b3b96a2b03c
vomkaraxioned <omkarv@axioned.com>
2022-09-12 15:24:07 +0530

    css added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,48 +2,27 @@
 
 */
 let box = $(".box");
-let colors = ["#ff0","#0ff","#f00","#800","#e06666","#f6b26b","#ff8c00"];
-let index  = 0;
-function animation () {
-    box.animate({ left:'90%'}, 2000);
-    box.animate({ top:0 }, 2000);
-    box.animate({ left: 0 }, 2000);
-    box.animate({top:'100%'}, 2000, ()=> {
+let colors = ["#ff0", "#0ff", "#f00", "#800", "#e06666", "#f6b26b", "#ff8c00"];
+let index = 0;
+
+function animation() {
+    box.animate({ left: '90%' }, 6000);
+    box.animate({ top: 0 }, 2000);
+    box.animate({ left: 0 }, 6000);
+    box.animate({ top: '100%' }, 2000, () => {
         animation();
     });
 }
 
 function changeColor() {
-    if(index >= colors.length){
+    if (index >= colors.length) {
         index = 0;
     }
-    box.css("backgroundColor",colors[index]);
-    index ++;
+    box.css({ "backgroundColor": colors[index] });
+    index++;
 }
 
-box.click(()=>{
-    setInterval(changeColor,500);
+box.click(() => {
+    setInterval(changeColor, 500);
     animation();
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
